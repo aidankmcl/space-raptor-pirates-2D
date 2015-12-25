d5c299b72c159b9da2866c2d2798dd28f83b0e6b
Aidan McLaughlin <success@Aidans-MacBook-Pro.local>
2015-12-24 22:19:09 -0600

    enemies chase and can be defeated by shooting

--- client/js/enemies.js
+++ client/js/enemies.js
@@ -1,8 +1,8 @@
-var calculateNextEnemyPosition = function ($player, enemy, bounds, timeDelta) {
-	var enemy = $(enemy);
+var ENEMY_SPEED = 200.0  // pixels per second
 
-	var currentX = parseInt(enemy.css('left'), 10);
-	var currentY = parseInt(enemy.css('top'), 10);
+var calculateNextEnemyPosition = function ($player, $enemy, bounds, timeDelta) {
+	var currentX = parseInt($enemy.css('left'), 10);
+	var currentY = parseInt($enemy.css('top'), 10);
 
 	var playerX = parseInt($player.css('left'), 10);
 	var playerY = parseInt($player.css('top'), 10);
@@ -13,11 +13,14 @@ var calculateNextEnemyPosition = function ($player, enemy, bounds, timeDelta) {
 
 	var distToHuman = Math.sqrt((dX * dX) + (dY * dY));
 
-	var deltaX = (dX/distToHuman) * 200.0 * timeDelta + (Math.random() * 1.25);
-	var deltaY = (dY/distToHuman) * 200.0 * timeDelta + (Math.random() * 1.25);
+	var deltaX = (dX/distToHuman) * ENEMY_SPEED * timeDelta + (Math.random() * 1.5);
+	var deltaY = (dY/distToHuman) * ENEMY_SPEED * timeDelta + (Math.random() * 1.5);
 
 	var newX = currentX + deltaX;
 	var newY = currentY + deltaY;
+
+	newX = (newX > bounds.right || newX < bounds.left)? currentX : newX;
+	newY = (newY > bounds.bottom || newY < bounds.top)? currentY : newY;
 
 	return {x: newX, y: newY}
 }
@@ -25,13 +28,52 @@ var calculateNextEnemyPosition = function ($player, enemy, bounds, timeDelta) {
 var moveEnemies = function (enemies, bounds, timeDelta) {
 	var $player = $('#player');
 
-	enemies.each(function(index, enemy) {
-		var nextPos = calculateNextEnemyPosition($player, enemy, bounds, timeDelta);
-		$(enemy).css('top', nextPos.y.toString()+"px");
-		$(enemy).css('left', nextPos.x.toString()+"px");
-	});
+	for (var id in enemies) {
+		var $enemy = $('#'+id);
+		var nextPos = calculateNextEnemyPosition($player, $enemy, bounds, timeDelta);
+
+		$enemy.css('top', nextPos.y.toString()+"px");
+		$enemy.css('left', nextPos.x.toString()+"px");
+	};
 }
 
-var Enemy = function() {
-	
+var Enemy = function(type, room) {
+	this.id = Math.random().toString(36).slice(-8);
+	var vents = [
+		{x: 110, y: 85}, // top left coordinates
+		{x: 385, y: 85}, // top right
+		{x: 110, y: 295}, // bottom right
+		{x: 385, y: 295} // bottom left
+	];
+
+	this.location = vents[Math.floor(Math.random()*4)];
+
+	this.src = "http://www.clker.com/cliparts/4/v/k/K/4/E/triceratops-hi.png";
+	this.x = this.location.x;
+	this.y = this.location.y;
+	this.health = 100;
+
+	this.type = type;
+	this.room = room;
+
+	this.init();
 };
+
+Enemy.prototype.init = function () {
+	$('#field').append('<img src="'+this.src+'" class="enemy" id="'+this.id+'" />');
+
+	$('#'+this.id).css('top', this.y.toString()+"px");
+	$('#'+this.id).css('left', this.x.toString()+"px");
+}
+
+Enemy.prototype.hurt = function (points) {
+	this.health -= points;
+	if (this.health <= 0) {
+		this.die();
+	}
+}
+
+Enemy.prototype.die = function () {
+	$('#'+this.id).remove();
+	delete enemies[this.id];
+}
--- server/server.js
+++ server/server.js
@@ -8,33 +8,9 @@ var config  = require('./config.json');
 
 app.use(express.static(__dirname + '/../client'));
 
-var KEY_W = 87,
-  KEY_A = 65,
-  KEY_S = 83,
-  KEY_D = 68;
-
 var players = {}
 
-// io.on('connection', function (socket) {
-//   console.log(socket.id + " connected!");
-
-//   socket.on('newPlayer', function(data) {
-//   	data = JSON.parse(data);
-//   	data.id = socket.id
-//   	players[socket.id] = data;
-//   	socket.broadcast.emit('playerJoined', JSON.stringify(data));
-//   })
-  
-//   socket.on('updatePos', function(data) {
-//   	data = JSON.parse(data);
-//   	socket.broadcast.emit('updateOthersPos', JSON.stringify(data));
-//   });
-
-//   socket.on('disconnect', function(data) {
-//   	console.log('hit disconnect', socket.id);
-//   	players = del(players, socket.id);
-//   });
-// });
+// io.on('connection', function (socket) {} );
 
 var serverPort = process.env.PORT || config.port;
 
