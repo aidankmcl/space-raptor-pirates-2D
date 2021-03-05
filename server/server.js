321fdc00726cd80d07fff0a12d06b1adeae86cd0
Aidan <aidankmcl@gmail.com>
2021-03-05 12:10:12 -0600

    fix security vulnerabilities

--- server/server.js
+++ server/server.js
@@ -1,16 +1,12 @@
 var express = require('express');
 var app     = express();
 var http    = require('http').Server(app);
-// var io      = require('socket.io')(http);
-var del = require('key-del');
 
 var config  = require('./config.json');
 
 app.use(express.static(__dirname + '/../client'));
 
 var players = {}
-
-// io.on('connection', function (socket) {} );
 
 var serverPort = process.env.PORT || config.port;
 
